779a07525880d993cbec340d23a718896de52d5a
Ejoz <chloe.laurent@ntymail.com>
2019-03-13 10:30:03 +0100

    règle le problème des continuous values

--- modeles.py
+++ modeles.py
@@ -11,23 +11,21 @@ from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_sc
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, f1_score
+import matplotlib.pyplot as plt
 
 #%% A transformer en importation mongo
 df = pd.read_csv('indeed_v4.csv', sep=',', index_col='Unnamed: 0', na_filter=True)
 df.info()
 df.isna().sum()
 
-#%% Pre-prcessing pour fitter le modèle
+#%% Pre-processing pour fitter le modèle
 data = df[['Bassin_emploi', 'Contrat', 'Poste', 'Salary']][df['Salary'].isna() == False]
 data = pd.get_dummies(data=data, columns={'Poste', 'Bassin_emploi', 'Contrat'}, drop_first=True)
 
 x = data.iloc[:, 1:]
-y = data['Salary']
+y = data['Salary'].astype('int64')
 
-lab_enc = LabelEncoder()
-y_encoded = lab_enc.fit_transform(y)
-
-x_train, x_test, y_train, y_test = train_test_split(x, y_encoded)
+x_train, x_test, y_train, y_test = train_test_split(x, y)
 
 #%% Kernel RBF
 rbf = SVC()
@@ -37,7 +35,12 @@ parameters = {'kernel': ['rbf'],
 clf_rbf = GridSearchCV(rbf, parameters, cv=5)
 clf_rbf.fit(x_train, y_train)
 clf_rbf.best_params_
+y_pred_rbf = clf_rbf.predict(x_test)
 
+# Comparaison données test/entraînement
+plt.scatter(range(len(y_test)), y_test, color = 'blue')
+plt.scatter(range(len(y_pred_rbf)), y_pred_rbf, color = 'red')
+plt.legend(('Training set', 'Test set'))
 
 #%% Random Forest
 rf = RandomForestClassifier()
@@ -47,8 +50,12 @@ parameters = grid_param = {'n_estimators': [100, 300, 500, 800, 1000],
 clf_rf = GridSearchCV(rf, parameters, cv=5)
 clf_rf.fit(x_train, y_train)
 clf_rf.best_params_
-y_pred = clf_rf.predict(x_test)
+y_pred_rf = clf_rf.predict(x_test)
 accuracy_score(y_test, y_pred)
 f1_score(y_test, y_pred, average='micro')
 all_accuracies = cross_val_score(estimator=clf_rf, X=x_train, y=y_train, cv=5)
 
+# Comparaison données test/entraînement
+plt.scatter(range(len(y_test)), y_test, color = 'blue')
+plt.scatter(range(len(y_pred_rf)), y_pred_rf, color = 'red')
+plt.legend(('Training set', 'Test set'))
