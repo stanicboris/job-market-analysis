afc673496b314128343b3a3ffa5f22fbce6848dc
Ejoz <chloe.laurent@ntymail.com>
2019-03-19 19:17:18 +0100

    ajout de dash_table

--- dash_test.py
+++ dash_test.py
@@ -7,26 +7,40 @@ Ceci est un script temporaire.
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table
 import pandas as pd
 
-df = pd.read_csv("indeed_v4.csv", sep=",")
+df = pd.read_csv("indeed_v4.csv", sep=",", index_col='Unnamed: 0')
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
+    html.H1(children="DB"),
+    dash_table.DataTable(
+    id='table',
+    columns=[{"name": i, "id": i} for i in df.columns],
+    data=df.to_dict("rows"),
+    style_table={
+        'maxHeight': '300',
+        'overflowY': 'scroll'
+    },
+    filtering=True,
+    editable=True
+)
+    ,
     html.H1(children='Rapport Indeed'),
 
     html.Div(children='''
-        Analyse de l'emploi à Paris, Lyon, Toulouse, Nantes et Bordeaux
+        Analyse de l'emploi à Paris, Lyon, Toulouse, Nantes, Montpellier et Bordeaux
     '''),
 
     dcc.Graph(
         id='example-graph',
         figure={
             'data': [
-                {'x': df['Bassin_emploi'], 'y': df['Salary'][df['Salary']<50000], 'type': 'bar', 'name': 'Semaine précédente'}
+                {'x': df['Bassin_emploi'], 'y': df['Salary'], 'type': 'bar', 'name': 'Semaine précédente'}
             ],
             'layout': {
                 'title': 'Répartition des offres d\'emploi'
@@ -42,8 +56,9 @@ app.layout = html.Div(children=[
         id='example-graph2',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'Semaine précédente'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Semaine actuelle'},
+                {'x': df['Bassin_emploi'], 'y': df['Salary'][df['Poste']=='Dev'], 'type': 'bar', 'name': u'Dev'},
+                {'x': df['Bassin_emploi'], 'y': df['Salary'][df['Poste']=='Data Scientist'], 'type': 'bar', 'name': u'Data Scientist'},
+                {'x': df['Bassin_emploi'], 'y': df['Salary'][df['Poste']=='Data Analyst'], 'type': 'bar', 'name': u'Data Analyst'}
             ],
             'layout': {
                 'title': 'Répartition des offres d\'emploi'
