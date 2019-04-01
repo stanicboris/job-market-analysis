d1fd0d9157c215500b03b625b13b0c90457deef6
goupilAnthony <anthony.93460@gmail.com>
2019-04-01 12:57:56 +0200

    cron

--- cron.py
+++ cron.py
@@ -1,11 +1,14 @@
-from crontab import CronTab
 
-cron = CronTab(user=True)
-job = cron.new('python3 /home/zanea/Projets/job-market-analysis/test_cron.py')
-job.minute.every(1)
-cron.write()
+def main():
+    from crontab import CronTab
+
+    cron = CronTab('zanea')
+    job = cron.new('python3 /home/zanea/Projets/job-market-analysis/test_cron.py')
+    job.minute.every(1)
+    cron.write()
 
 
 
 
-
+if __name__ == "__main__":
+  main()
