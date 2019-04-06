e4c0cfcd7aa2833515f562a274d1f17639e37596
goupilAnthony <anthony.93460@gmail.com>
2019-04-06 13:14:31 +0200

    salary int

--- show_db_len.py
+++ show_db_len.py
@@ -1,6 +1,16 @@
 import mongo
+import pandas as pd
 # Importation de la base de données en dataframe
 mongo = mongo.Mongo()
 
 df = mongo.get_df()
 print('Longueur Database = ',len(df)) 
+
+
+print(df['Salary'].dtypes)
+
+df['Salary'] = pd.to_numeric(df['Salary'])
+
+print(df['Salary'].dtypes)
+
+df.to_csv('indeed_v5.csv')
